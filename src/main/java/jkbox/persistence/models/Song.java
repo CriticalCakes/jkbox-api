91ea5ec2a65adc2417a884711c33b7eb1a1c17e1
Lincon Dias <lincondiasribeiro@gmail.com>
2017-11-16 20:19:39 -0200

    Criado modelo do Song básico, sem o relacionamento

---
+++ src/main/java/jkbox/persistence/models/Song.java
@@ -0,0 +1,42 @@
+package jkbox.persistence.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Song {
+	
+	@Id
+	private Long id;
+	private String title;
+	private String url;
+	private boolean playing;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public boolean isPlaying() {
+		return playing;
+	}
+	public void setPlaying(boolean playing) {
+		this.playing = playing;
+	}
+	
+	
+}
