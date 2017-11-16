d92af90e359ab792d5585e20d9cbd97362e3ee38
Lincon Dias <lincondiasribeiro@gmail.com>
2017-11-16 20:19:53 -0200

    Criado os DAO's dos modelos

---
+++ src/main/java/jkbox/persistence/dao/PlaylistDAO.java
@@ -0,0 +1,24 @@
+package jkbox.persistence.dao;
+
+public class PlaylistDAO {
+	
+	public void create() {
+		
+	}
+	
+	public void update(Long id) {
+		
+	}
+	
+	public void delete(Long id) {
+		
+	}
+	
+	public void get(Long id) {
+		
+	}
+	
+	public void getAll() {
+		
+	}
+}
---
+++ src/main/java/jkbox/persistence/dao/SongDAO.java
@@ -0,0 +1,26 @@
+package jkbox.persistence.dao;
+
+public class SongDAO {
+	
+	public void create() {
+		
+	}
+	
+	public void update(Long id) {
+		
+	}
+	
+	public void delete(Long id) {
+		
+	}
+	
+	public void get(Long id) {
+		
+	}
+	
+	public void getAll() {
+		
+	}
+	
+	
+}
