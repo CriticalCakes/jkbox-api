547d6ad290d64917edf68ea26f8ad4c6c266fe7c
Lincon Dias <lincondiasribeiro@gmail.com>
2017-11-24 20:42:20 -0200

    Adicionado funcionalidades de atualizar e deletar

--- src/main/java/jkbox/api/endpoints/PlaylistEndpoint.java
+++ src/main/java/jkbox/api/endpoints/PlaylistEndpoint.java
@@ -1,48 +1,61 @@
 package jkbox.api.endpoints;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import jkbox.persistence.dao.PlaylistDAO;
 import jkbox.persistence.models.Playlist;
-import jkbox.services.youtube.MetadataModel;
-import jkbox.services.youtube.YoutubeService;
 
 @Path("/playlist")
 public class PlaylistEndpoint {
+	
+	private static PlaylistDAO dao = new PlaylistDAO();
 	
 	@POST
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response create(Playlist p) {
-		PlaylistDAO dao = new PlaylistDAO();
-		dao.create(p);
-		return Response.status(201).entity(p).build();
+		Playlist created = dao.create(p);
+		return Response.status(201).entity(created).build();
 	}
 	
-	public void update(Long id) {
-		
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response update(@PathParam("id") Long id, Playlist pl) {
+		Playlist updated = dao.update(id, pl);
+		return Response.status(200).entity(updated).build();
 	}
 	
-	public void delete(Long id) {
-		
+	@DELETE
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response delete(@PathParam("id")Long id) {
+		dao.delete(id);
+		return Response.status(204).build();
+	}
+	
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response get(@PathParam("id") Long id) {
+		Playlist pl = dao.get(id);
+		return Response.status(200).entity(pl).build();
 	}
 	
 	@GET
 	@Path("/")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response get(@QueryParam("url") String url) {
-		MetadataModel mm = YoutubeService.getVideoInfo(url);
-		return Response.status(200).entity(mm).build();
-	}
-	
 	public void getAll() {
 		
 	}
--- src/main/java/jkbox/persistence/dao/PlaylistDAO.java
+++ src/main/java/jkbox/persistence/dao/PlaylistDAO.java
@@ -10,27 +10,51 @@ public class PlaylistDAO {
 	
 	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("jkbox");
 	
-	public void create(Playlist p) {
+	public Playlist create(Playlist p) {
 		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
 		
 		// Faz a persistencia do modelo
+		// TODO tratar exceções na criação
 		em.persist(p);
+		
+		em.getTransaction().commit();
+		em.close();
+		return p;
+	}
+	
+	public Playlist update(Long id, Playlist pl) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		
+		pl.setId(id);
+		em.merge(pl);
+		
+		em.getTransaction().commit();
+		em.close();
+		return pl;
+	}
+	
+	public void delete(Long id) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		
+		em.remove(em.getReference(Playlist.class, id));
 		
 		em.getTransaction().commit();
 		em.close();
 	}
 	
-	public void update(Long id) {
+	public Playlist get(Long id) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
 		
-	}
-	
-	public void delete(Long id) {
+		// Consulta o objeto
+		// TODO tratar exceções na busca
+		Playlist pl = em.find(Playlist.class, id);
 		
-	}
-	
-	public void get(Long id) {
-		
+		em.close();
+		return pl;
 	}
 	
 	public void getAll() {
