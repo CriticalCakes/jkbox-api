9964d934a06f2d3511cab48abf1cd08485419d77
Pedro Henrique <pedro0798henrique@gmail.com>
2017-11-30 21:43:49 -0200

    Documentação da DAO Song.

--- src/main/java/jkbox/persistence/dao/SongDAO.java
+++ src/main/java/jkbox/persistence/dao/SongDAO.java
@@ -1,26 +1,46 @@
 package jkbox.persistence.dao;
 
+/**
+ * Persistência de dados da Song.
+ * @author Lincon Dias e Pedro Henrique Fernandes.
+ *
+ */
 public class SongDAO {
 	
-	public void create() {
+	/**
+	 * Faz a persistência de uma Song.
+	 * @param s Song que será persistida.
+	 * @return Song persistida.
+	 */
+	public void create(Song s) {
 		
 	}
 	
+	/**
+	 * Edita uma Song especificada pelo id.
+	 * @param id Long id da Song que será editada.
+	 * @param pl Song é uma entidade preenchida com novos dados.
+	 * @return Song atualizada e persistida.
+	 */	
 	public void update(Long id) {
 		
 	}
 	
+	/**
+	 * Exclui uma Song especificada pelo id.
+	 * @param id Long id da Song que será excluída.
+	 */
 	public void delete(Long id) {
 		
 	}
 	
+	/**
+	 * Retorna uma Song especificada pelo id.
+	 * @param id Long id da Song que será retornada.
+	 * @return Song especificada.
+	 */
 	public void get(Long id) {
 		
 	}
-	
-	public void getAll() {
-		
-	}
-	
-	
+			
 }
